51bf3c347927625667e66779f43d369cd76dc5af
Alec Garza <alecgarza96@gmail.com>
2022-01-20 10:23:59 -0600

    Added test for _checkCredentialsExpired()

--- SmartMirror/tests_calendar.py
+++ SmartMirror/tests_calendar.py
@@ -2,5 +2,15 @@ from Calendar import GoogleCalendar
 
 TOKEN = '/Users/alecgarza/Desktop/SmartMirrorBackup/cfg/token.json'
 cal = GoogleCalendar()
+
+print("Unit Testing for GoogleCalendar")
+print("-------------------------------")
+
+print("Testing: _getCreds()")
 cal._getCreds(TOKEN)
-print(cal.credentials)
+print("Credentials Found:", cal.credentials)
+print('\n')
+
+print("Testing: _checkCredentialsExpired()")
+print("Credentials Expiration Status:", cal._checkCredentialsExpired())
+print('\n')
