db5150693ee8c28d402398f9bb22f9c52a98f987
Alec Garza <alecgarza96@gmail.com>
2022-01-27 20:03:47 -0600

    wrote simple GUI class to build off of

---
+++ SmartMirror/SmartMirror.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+class SmartMirror(tk.Frame):
+
+	def __init__(self,master=None):
+		tk.Frame.__init__(self,master)
+		self.master = master
+		self.place(relwidth=1,relheight=1)
+
+		text = tk.Label(self, text="Just do it")
+		text.place(x=70,y=90)
+
+
+root = tk.Tk()
+smartMirror = SmartMirror(root)
+root.mainloop()
