8de9146256180123d0ba01a196b21f9734939528
Alec Garza <alecgarza96@gmail.com>
2022-02-03 13:17:05 -0600

    wrote Quotes class methods, implemented in main

--- SmartMirror/Quotes.py
+++ SmartMirror/Quotes.py
@@ -2,21 +2,32 @@ import json
 
 class Quotes():
 
-	def __init__(self, quotes=None):
-		self.today = " "
-		self.quotesDict = quotes
+	def __init__(self):
+		self.quote = " "
+		self.quotesJson = None
+		self.currentCounter = 0
+
+	def getQuotesJson(self, quotesJson):
+		file = open(quotesJson)
+		self.quotesJson = json.load(file)
+		file.close()
+
+	def _checkCounter(self):
+		if(self.currentCounter == len(self.quotesJson)):
+			self.currentCounter == 0
+
+	def _setQuote(self):
+		if(self.quotesJson):
+			self.quote = self.quotesJson['quotes'][self.currentCounter]
+			self.currentCounter += 1
+
+	def setTodaysQuote(self):
+		self._checkCounter()
+		self._setQuote()
+
+	def getTodaysQuote(self):
+
+		return self.quote
 
 
 
-file = open('quotes.json')
-
-quotes = json.load(file)
-
-"""
-for i in quotes['quotes']:
-	print(i)
-	"""
-
-print(quotes['quotes'][0])
-
-print(len(quotes['quotes']))
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from SmartMirror.Location import Location
 from SmartMirror.Time import Time
 from SmartMirror.Date import Date
 from SmartMirror.SmartMirror import SmartMirror
+from SmartMirror.Quotes import Quotes
 
 if __name__ == '__main__':
 
@@ -14,16 +15,22 @@ if __name__ == '__main__':
 	city = loc.getCity()
 	state = loc.getState()
 	weather = Weather()
+	quote = Quotes()
+	quote.getQuotesJson('SmartMirror/quotes.json')
+	quote.setTodaysQuote()
+
 
 	temperature = weather.get_temp(city,state) + " degrees"
 	weatherReport = weather.get_report(city,state)
 	time = Time().get_time()
 	date = Date().get_date()
+	quoteText = quote.getTodaysQuote()['quote']
+	quoteAuthor = quote.getTodaysQuote()['author']
 
 	root = tk.Tk()
 	canvas = tk.Canvas(root, height=800, width=900)
 	canvas.pack()
-	smartMirror = SmartMirror(root,temperature,"quote","word",time,date)
+	smartMirror = SmartMirror(root,temperature,quoteText+" - "+quoteAuthor,"word",time,date)
 	root.mainloop()
 
 
